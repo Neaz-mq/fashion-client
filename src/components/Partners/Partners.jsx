593029f5fb644ad9f0261a22061558e4a09e1bfa
Neaz-mq <neazmorshed666@gmail.com>
2023-10-20 18:32:29 +0600

    added dark light theme

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -3,15 +3,15 @@
 const Partners = () => {
     return (
         <section className="sponsors text-center" data-aos="fade-up">
-            <h3 className="section-title lg:text-5xl text-xl font-bold mt-20"> Our Partners </h3>
+            <h3 className="section-title lg:text-5xl text-4xl font-bold mt-20 mb-4 lg:mb-0"> Our Partners </h3>
 
             <div className="sponsors-img-container flex justify-between items-center lg:gap-24 flex-col  lg:flex-row ">
                 
-                <img className="-mt-12 lg:mt-0" src="https://i.ibb.co/VHPzHTh/New-Movenpick-Logo-1.jpg" alt="" />
+                <img className="-mt-12 lg:mt-0" src="https://i.ibb.co/JCWXTFR/New-Movenpick-Logo-1-removebg-preview.png" alt="" />
                 
-                <img className="-mt-20 lg:mt-0" src="https://i.ibb.co/ftShpTD/mcgettiganslogo.jpg" alt="" />
-                <img className="-mt-12 lg:mt-0" src="https://i.ibb.co/KVnmWX9/169.jpg" alt="" />
-                <img src="https://i.ibb.co/BcBxVgM/corplogo-miyagis.jpg" alt="" />
+                <img className="-mt-20 lg:mt-0" src="https://i.ibb.co/kmr84DX/169-removebg-preview.png" alt="" />
+                <img className="-mt-12 lg:mt-0" src="https://i.ibb.co/rxfWngp/mcgettiganslogo-removebg-preview.png" alt="" />
+                <img src="https://i.ibb.co/W2GkYwm/corplogo-miyagis-removebg-preview.png" alt="" />
 
 
 
